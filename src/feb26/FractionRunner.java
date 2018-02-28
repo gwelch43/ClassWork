db71a002e1c3c73f7a6549b27f2eb82da3892f87
gwelch43 <garrettwelch39@yahoo.com>
2018-02-28 08:59:33 -0600

    Worked on fraction runner and casted o.

--- src/feb26/FractionRunner.java
+++ src/feb26/FractionRunner.java
@@ -6,6 +6,11 @@ public class FractionRunner
     {
         Fraction f1 = new Fraction (1, 4);
         Fraction f2 = new Fraction (1, 3);
+        Fraction f3 = f1;
         System.out.println(f1.compareTo(f2));
+        String s1 = "Frog";
+        String s2 = "Frog";
+        System.out.println(s1==s2);
+        System.out.println(f1==f3);
     }
 }
